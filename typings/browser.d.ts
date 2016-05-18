19a7eb2321ca43649848e39970246f54642233fe
Thierry Templier <templth@yahoo.fr>
2016-05-18 09:22:33 +0200

    Fixed compilation errors

--- typings/browser.d.ts
+++ typings/browser.d.ts
@@ -1,2 +1,1 @@
-/// <reference path="browser/ambient/es6-promise/es6-promise.d.ts" />
 /// <reference path="browser/ambient/es6-shim/es6-shim.d.ts" />
--- typings/main.d.ts
+++ typings/main.d.ts
@@ -1,2 +1,1 @@
-/// <reference path="main/ambient/es6-promise/es6-promise.d.ts" />
 /// <reference path="main/ambient/es6-shim/es6-shim.d.ts" />
